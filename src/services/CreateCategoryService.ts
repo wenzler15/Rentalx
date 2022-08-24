c0804554c6bde18e71fe9f35e6f0da9e68db534d
DouglasWenzler <douglas.wenzler@hogarthww.com>
2022-08-24 20:22:51 -0300

    SOLID

---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,29 @@
+import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
+
+interface IRequest {
+    name: string;
+    description: string;
+}
+
+/**
+ * [x] - Definir o tipo de retorno
+ * [x] - Alterar retorno de erro
+ * [x] - Acessar o repositório
+ */
+
+class CreateCategoryService {
+    constructor(private categoriesRepository: ICategoriesRepository) { }
+
+    execute({ description, name }: IRequest): void {
+        const categoryAlreadyExists =
+            this.categoriesRepository.findByName(name);
+
+        if (categoryAlreadyExists) {
+            throw new Error("Category alteady exists!");
+        }
+
+        this.categoriesRepository.create({ name, description });
+    }
+}
+
+export { CreateCategoryService };
