e53bd5b31e58a18955171c9db01f61ec2e52ef20
DougMobitype <douglas.wenzler@mobitype.com.br>
2022-11-08 19:51:11 -0300

    finish class

---
+++ src/modules/cars/repositories/SpecificationsRepository.ts
@@ -0,0 +1,34 @@
+import { Specification } from "../model/Specification";
+import {
+    ICreateSpecificationDTO,
+    ISpecificationRepository,
+} from "./ISpecificationsRepository";
+
+class SpecificationsRepository implements ISpecificationRepository {
+    private specifictions: Specification[];
+
+    constructor() {
+        this.specifictions = [];
+    }
+
+    create({ description, name }: ICreateSpecificationDTO): void {
+        const specification = new Specification();
+
+        Object.assign(specification, {
+            name,
+            description,
+            created_at: new Date(),
+        });
+
+        this.specifictions.push(specification);
+    }
+
+    findByName(name: string): Specification {
+        const specification = this.specifictions.find(
+            specification => specification.name === name,
+        );
+        return specification;
+    }
+}
+
+export { SpecificationsRepository };
--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,24 @@
+import { ISpecificationRepository } from "../repositories/ISpecificationsRepository";
+
+interface IRequest {
+    name: string;
+    description: string;
+}
 class CreateSpecificationService {
-    execute() {
-        console.log("oi");
+    constructor(private specificationsRepository: ISpecificationRepository) { }
+
+    execute({ name, description }: IRequest): void {
+        const specificationAlreadyExists =
+            this.specificationsRepository.findByName(name);
+
+        if (specificationAlreadyExists) {
+            throw new Error("Specification already exists!");
+        }
+
+        this.specificationsRepository.create({
+            name,
+            description,
+        });
     }
 }
 
