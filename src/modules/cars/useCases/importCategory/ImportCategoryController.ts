481bb2e0001f42418c47c44a1f2b063f8f7d0756
wenzler <douglas_wenzler@hotmail.com>
2022-11-21 20:53:46 -0300

    adding multer

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+import { Request, Response } from "express";
+
+import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
+
+class ImportCategoryController {
+    constructor(private importCategoryUseCase: ImportCategoryUseCase) { }
+
+    handler(request: Request, response: Response): Response {
+        const { file } = request;
+
+        this.importCategoryUseCase.execute(file);
+
+        return response.send();
+    }
+}
+
+export { ImportCategoryController };
---
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,8 @@
+class ImportCategoryUseCase {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    execute(file: any) {
+        console.log(file);
+    }
+}
+
+export { ImportCategoryUseCase };
